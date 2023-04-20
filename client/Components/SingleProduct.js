44f2abf3fc212cca8e691103bcbd8cc096202165
jsr09 <97914865+jsr09@users.noreply.github.com>
2023-04-20 15:21:10 -0400

    Updated import routes for new file structure

--- client/Components/SingleProduct.js
+++ client/Components/SingleProduct.js
@@ -1,20 +1,20 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
-import { fetchSingleProduct } from "../app/SingleProductSlice";
+import React, { useEffect, useState } from "react";
+
+// import { useDispatch, useSelector } from "react-redux";
+// import { useParams } from "react-router-dom";
+// import { fetchSingleProduct } from "../app/SingleProductSlice";
 
 function SingleProduct() {
-  const { id } = useParams();
-  const dispatch = useDispatch();
-  const product = useSelector((state) => state.SingleProduct);
+  // const { id } = useParams();
+  // const dispatch = useDispatch();
+  // const product = useSelector((state) => state.SingleProduct);
 
-  useEffect(
-    () => {
-      dispatch(fetchSingleProduct(id));
-    },
-    [dispatch],
-    id
-  );
+  // useEffect(
+  //   () => {
+  //     dispatch(fetchSingleProduct(id));
+  //   },
+  //   [],
+  // );
 
   return (
     <div>
