0b467b3a6f1a92c96a780445503991b736eb7b70
jsr09 <97914865+jsr09@users.noreply.github.com>
2023-04-24 18:35:46 -0400

    Whoops made changes to main brance.  Updates routes with try catch blocks finished testing all routes.  Need special routes for changing order enum and deleting cart assocciated with user.

--- server/api/carts.js
+++ server/api/carts.js
@@ -4,53 +4,63 @@ const Cart = require('../db/models/Cart')
 const { userAuth, adminAuth } = require('../middlewares/authorize')
 
 // Get/api/carts/
-// Gets all carts
-//!tested
 router.get('/', adminAuth, async (req, res, next) => {
+    try {
     let user = req.user
     if (!user) return res.sendStatus(401)
     const cart = await Cart.findAll()
     res.json(cart)
+    } catch (err) {
+        next(err)
+    }
 })
-
 //Get/api/cart/id
-//!tested good!
 router.get('/:userId', userAuth, async (req, res, next) => {
+    try {
     let user = req.user
     const carts = await Cart.findByPk(user.id)
     res.json(carts)
+    } catch (err) {
+        next(err)
+    }
 })
-
 //Post/api/cart/
-//User only
-//!test good!
 router.post('/', userAuth, async (req, res, next) => {
+    try {
     let user = req.user
     if (!user) return res.sendStatus(401)
     // console.log('line 28 reaq.body>>>>', req.body)
     const cart = await Cart.create(req.body)
     res.json(cart)
+    } catch (err) {
+        next(err)
+    }
 })
-
 //Put/api/cart/id
-//update cart
-//!test good!
-router.put('/:id', userAuth, async (req, res, next) => {
+router.put('/:userid', userAuth, async (req, res, next) => {
+    try {
+    let user = req.user
+    if (!user) return res
     const cart = await Cart.findByPk(req.params.id)
     const cartUpdate = await cart.update(req.body)
     res.json(cartUpdate)
+    } catch (err) {
+        next(err)
+    }
 })
 
 //Delete/api/orders/id
-//delete cart
-//!test good!
-router.delete('/:id', async (req, res, next) => {
+router.delete('/:userid', async (req, res, next) => {
+    try {
     const cart = await Cart.destroy({
         where: {
             id: req.params.id,
         },
     })
     res.send({ message: `${cart} has been destroyed` })
+    } catch (err) {
+        next(err)
+    }
 })
 
 module.exports = router
--- server/api/orders.js
+++ server/api/orders.js
@@ -4,56 +4,60 @@ const Order = require('../db/models/Order')
 const { userAuth, adminAuth } = require('../middlewares/authorize')
 
 // Get/api/orders/
-// Gets all orders
-//!tested good
 router.get('/', adminAuth, async (req, res, next) => {
-    let user = req.user
-    if (!user) return res.sendStatus(401)
-    const order = await Order.findAll()
-    res.json(order)
+    try {
+        let user = req.user
+        if (!user) return res.sendStatus(401)
+        const order = await Order.findAll()
+        res.json(order)
+    } catch (error) {
+        next(error)
+    }
 })
-
 //Get/api/orders/id
-//!tested good
-//todo get all orders from a customer id
 router.get('/:userId', userAuth, async (req, res, next) => {
-    let user = req.user
-    const orders = await Order.findByPk(user.id)
-    res.json(orders)
+    try {
+        let user = req.user
+        const orders = await Order.findByPk(user.id)
+        res.json(orders)
+    } catch (error) {
+        next(error)
+    }
 })
-
 //Post/api/orders/
-//User only
-//!test good!
 router.post('/', userAuth, async (req, res, next) => {
-    let user = req.user
-    if (!user) return res.sendStatus(401)
-    // console.log('line 28 reaq.body>>>>', req.body)
-    const order = await Order.create(req.body)
-    res.json(order)
+    try {
+        let user = req.user
+        if (!user) return res.sendStatus(401)
+        // console.log('line 28 reaq.body>>>>', req.body)
+        const order = await Order.create(req.body)
+        res.json(order)
+    } catch (error) {
+        next(error)
+    }
 })
-
 //Put/api/orders/id
-//update order
-//!test good!
 router.put('/:id', userAuth, async (req, res, next) => {
-    const order = await Order.findByPk(req.params.id)
-    const orderUpdate = await order.update(req.body)
-    res.json(orderUpdate)
+    try {
+        const order = await Order.findByPk(req.params.id)
+        const orderUpdate = await order.update(req.body)
+        res.json(orderUpdate)
+    } catch (error) {
+        next(error)
+    }
 })
-
 //Delete/api/orders/id
-//delete order
-//!test good!
-router.delete('/:id', async (req, res, next) => {
-    const order = await Order.destroy({
-        where: {
-            id: req.params.id,
-        },
-    })
-    res.send({ message: `${order} has been destroyed` })
+router.delete('/:id', userAuth, async (req, res, next) => {
+    try {
+        const order = await Order.destroy({
+            where: {
+                id: req.params.id,
+            },
+        })
+        res.send({ message: `${order} has been destroyed` })
+    } catch (error) {
+        next(error)
+    }
 })
-
-//todo put to change order state to checkout
 
 module.exports = router
